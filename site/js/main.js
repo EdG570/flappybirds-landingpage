d41e63ea4fc3ad33067e68227eafafe142e32fe8
Ed Glazenski <edglazenski@gmail.com>
2015-10-28 01:27:41 -0400

    Styles nearly finished.

--- site/js/main.js
+++ site/js/main.js
@@ -1,1 +1,18 @@
-var test = "hi";
+$(document).ready(function(){
+ 
+ function loop(){
+  $('#flappy').show();
+  $('#flappy').animate({
+      left: "+=3000",
+      
+    }, 10000, function(){
+      $('#flappy').hide();
+      $('#flappy').css('left', '0');
+      loop();
+  });
+
+ }
+
+ loop();
+
+});
